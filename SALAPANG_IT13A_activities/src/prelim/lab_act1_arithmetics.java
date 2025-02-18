35471b9be4389e3c2961e59991dcb61c50ceb0cf
Giancyril <mijaresgiancyril@gmail.com>
2025-02-18 08:10:51 +0800

    arithmetic

---
+++ SALAPANG_IT13A_activities/src/prelim/lab_act1_arithmetics.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prelim;
+
+/**
+ *
+ * @author User
+ */
+public class lab_act1_arithmetics {
+    
+   public static void main(String[] args) {
+       int A = 10;
+       int B = 4;
+       int C = 6;
+       int D = 3;
+       
+       int beth = A * B + C;
+       int pril = (A - B) % C;
+       int gab = (A + B + C) / D;
+       int john = A * C - (B * B);
+       
+       System.out.println("10 * 4 + 6 = " + beth);
+       System.out.println("(10 - 4) % 6 = " + pril);
+       System.out.println("(10 + 4 + 6) / 3 = " + gab);
+       System.out.println("10 * 6 - (4 * 4) = + john");
+      
+      
+   }
+}
